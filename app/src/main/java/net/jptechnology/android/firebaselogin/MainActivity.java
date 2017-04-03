b4fb52974941235032057789f07323cfcffe6802
John <jparkteach@yahoo.com>
2017-04-03 19:50:42 -0400

    FirebaseLogin

--- app/src/main/java/net/jptechnology/android/firebaselogin/MainActivity.java
+++ app/src/main/java/net/jptechnology/android/firebaselogin/MainActivity.java
@@ -1,13 +1,55 @@
 package net.jptechnology.android.firebaselogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null)
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+//                  Intent i = new Intent(MainActivity.this, LoginActivity.class);
+//                  startActivity(i);
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+    public void addSomething(View view) {
+        FirebaseUser user = auth.getCurrentUser();
+        DatabaseReference userRef = database.getReference(user.getUid());
+        userRef.push().setValue(Math.random());
     }
 }
